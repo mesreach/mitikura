d0bbf9410e3a5b0131f92aa363e3cb595e584614
htamakos <tmkshrnr@gmail.com>
2018-04-28 12:36:51 +0900

    fix some

--- michikusa/RangeAndGenreViewController.swift
+++ michikusa/RangeAndGenreViewController.swift
@@ -7,29 +7,20 @@
 import UIKit
 import GoogleMaps
 
-class RangeAndGenreViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    @IBOutlet weak var picker01: UIPickerView!
-    @IBOutlet weak var picker02: UIPickerView!
-
-    var picker01_data:[String] = [String]()
-    var picker02_data:[String] = [String]()
-    
+class RangeAndGenreViewController: UIViewController {
     var mapView : GMSMapView!
     let latitude: CLLocationDegrees = 35.531064
     let longitude: CLLocationDegrees = 139.684389
     
+    @IBOutlet weak var picker_genre: PickerKeyboard!
+    @IBOutlet weak var picker_km: PickerKeyboard!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        initPickerData()
+        picker_genre.addData(d: ["hoge", "hage"])
+        picker_km.addData(d: ["1", "2", "3"])
         
-        self.picker01.selectRow(0, inComponent: 0, animated: true)
-        self.picker02.selectRow(0, inComponent: 0, animated: true)
-        self.picker01.delegate = self
-        self.picker02.delegate = self
-        
-        self.picker01.showsSelectionIndicator = true;
-        self.picker02.showsSelectionIndicator = true;
         // mapview の描画サイズ
         let width: CGFloat = self.view.frame.maxX
         let height: CGFloat = self.view.frame.maxY / 2.5
@@ -48,47 +39,16 @@ class RangeAndGenreViewController: UIViewController, UIPickerViewDelegate, UIPic
         
         self.view.addSubview(mapView)
         self.view.sendSubview(toBack: mapView)
-        
-    }
-    
-    func initPickerData() {
-        self.picker01.tag = 1
-        self.picker02.tag = 2
-        self.picker01_data = ["1", "2", "3"]
-        self.picker02_data = ["hoge", "hoge2", "hoge3"]
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView.tag == 1 {
-            return picker01_data.count
-        } else {
-            return picker02_data.count
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView.tag == 1 {
-            return picker01_data[row]
-        } else {
-            return picker02_data[row]
-        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func goNext_(_ sender: UIButton) {
         let next = storyboard!.instantiateViewController(withIdentifier: "RangeAndGenre")
         self.present(next, animated: true, completion: nil)
     }
-    
-    
 }
 
 
