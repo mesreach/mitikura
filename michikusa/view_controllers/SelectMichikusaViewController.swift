b22374ddaeab152c873927703e6bbb5e2e247a70
r-waki <r.wakeyii@gmail.com>
2018-05-20 21:45:01 +0900

    JSONのパースの途中です

--- michikusa/view_controllers/SelectMichikusaViewController.swift
+++ michikusa/view_controllers/SelectMichikusaViewController.swift
@@ -16,6 +16,7 @@ class SelectMichikusaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.michikusaSpot.addData(d: self.data)
+        print ("michikusaRange2:\(michikusaRange!)")
         drawMap()
         //GooglePlace検索＆マップ表示
         getPlaceList(location:self.previousMarker!.position,mapView:mapView)
@@ -40,18 +41,28 @@ class SelectMichikusaViewController: UIViewController {
     func getPlaceList (location: CLLocationCoordinate2D, mapView:GMSMapView) {
     //func getPlaceList (mapView:GMSMapView) {
         //検索条件の指定
-        let location = [35.529792,139.698568] //ひとまずリクエストの中心は固定で試す。。
+        //let latitude: CLLocationDegrees = 35.531064
+        //let longitude: CLLocationDegrees = 139.684389
+
+        let location = "35.529792,139.698568" //ひとまずリクエストの中心は固定で試す。。
         let range = 500
         let type = "cafe"
-        //GooglePlaceAPIにリクエスト&JSONパース
+        let key = "AIzaSyB2VMdWQslynrxRgCA3vpWQAjqxvkKmCWk"
+
         let url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-        Alamofire.request(url,parameters:["location":location,"radius":range,"type":type]).responseJSON { response in
-            let json = try! JSON(data: response.data!)
-            let placeList = json["result"].arrayValue
+        Alamofire.request(url,parameters:["key":key, "location":location, "radius":range,"type":type]).responseJSON { response in
+            //let json = try! JSON(data: response.data!)
+            let json = JSON(response.result.value!)
+            print(response.result.value!)
+            json["result"].forEach{(_,data) in
+                let name = data["name"].string!
+                print(name)
+            }
+            //let placeList = json["result"].arrayValue
             print(response.result.description)
-            //print("routes count: \(routes.count)")
+            //print (placeList)
             //APIでの取得結果をマップ上にプロット
-            for place in placeList {
+            /* for place in placeList {
                 var marker :GMSMarker
                 var geometry = place["geometry"].dictionary
                 let placeLat = geometry?["lat"]?.doubleValue
@@ -59,6 +70,7 @@ class SelectMichikusaViewController: UIViewController {
                 marker = GMSMarker(position: CLLocationCoordinate2D(latitude: placeLat!, longitude: placeLng!))
                 marker.map = mapView
             }
+            */
         }
     }
 }
