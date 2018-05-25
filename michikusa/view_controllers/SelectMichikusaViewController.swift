6e910796004557dbc1c2aa75ac433a4eed27c3bb
r-waki <r.wakeyii@gmail.com>
2018-05-25 23:38:37 +0900

    パースはできたので、あとはマップにプロットの部分

--- michikusa/view_controllers/SelectMichikusaViewController.swift
+++ michikusa/view_controllers/SelectMichikusaViewController.swift
@@ -16,10 +16,11 @@ class SelectMichikusaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.michikusaSpot.addData(d: self.data)
-        print ("michikusaRange2:\(michikusaRange!)")
         drawMap()
         //GooglePlace検索＆マップ表示
-        getPlaceList(location:self.previousMarker!.position,mapView:mapView)
+        print ("positon:\(self.previousMarker!.position)")
+        
+        getPlaceList(location:self.previousMarker!.position, range:michikusaRange! ,mapView:mapView)
         //getPlaceList(mapView:mapView)
     }
     
@@ -38,39 +39,23 @@ class SelectMichikusaViewController: UIViewController {
     }
     
     
-    func getPlaceList (location: CLLocationCoordinate2D, mapView:GMSMapView) {
-    //func getPlaceList (mapView:GMSMapView) {
-        //検索条件の指定
-        //let latitude: CLLocationDegrees = 35.531064
-        //let longitude: CLLocationDegrees = 139.684389
-
-        let location = "35.529792,139.698568" //ひとまずリクエストの中心は固定で試す。。
-        let range = 500
+    func getPlaceList (location: CLLocationCoordinate2D, range:Int, mapView:GMSMapView) {
+        let centerPoint = "\(location.latitude),\(location.longitude)"
+        let range = 1000 * range
         let type = "cafe"
         let key = "AIzaSyB2VMdWQslynrxRgCA3vpWQAjqxvkKmCWk"
 
         let url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-        Alamofire.request(url,parameters:["key":key, "location":location, "radius":range,"type":type]).responseJSON { response in
-            //let json = try! JSON(data: response.data!)
+        Alamofire.request(url,parameters:["key":key, "location":centerPoint, "radius":range,"type":type]).responseJSON { response in
             let json = JSON(response.result.value!)
-            print(response.result.value!)
-            json["result"].forEach{(_,data) in
-                let name = data["name"].string!
-                print(name)
-            }
-            //let placeList = json["result"].arrayValue
-            print(response.result.description)
-            //print (placeList)
-            //APIでの取得結果をマップ上にプロット
-            /* for place in placeList {
-                var marker :GMSMarker
-                var geometry = place["geometry"].dictionary
-                let placeLat = geometry?["lat"]?.doubleValue
-                let placeLng = geometry?["lng"]?.doubleValue
-                marker = GMSMarker(position: CLLocationCoordinate2D(latitude: placeLat!, longitude: placeLng!))
+            json["results"].forEach{(_,place) in
+                let name = place["name"].string!
+                print (name)
+                let placeLat = place["geometry"]["location"]["lat"].doubleValue
+                let placeLng = place["geometry"]["location"]["lng"].doubleValue
+                let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: placeLat, longitude: placeLng))
                 marker.map = mapView
             }
-            */
         }
     }
 }
