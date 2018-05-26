8dbe7c975ced54e94df6c00cabf86bd35a23667d
r-waki <r.wakeyii@gmail.com>
2018-05-26 18:45:05 +0900

    plot some michikusa spot!!

--- michikusa/view_controllers/SelectMichikusaViewController.swift
+++ michikusa/view_controllers/SelectMichikusaViewController.swift
@@ -16,12 +16,8 @@ class SelectMichikusaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.michikusaSpot.addData(d: self.data)
-        drawMap()
         //GooglePlace検索＆マップ表示
-        print ("positon:\(self.previousMarker!.position)")
-        
         getPlaceList(location:self.previousMarker!.position, range:michikusaRange! ,mapView:mapView)
-        //getPlaceList(mapView:mapView)
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,20 +28,13 @@ class SelectMichikusaViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    func drawMap() {
-        if self.previousMarker != nil {
-            self.previousMarker!.map = self.mapView
-        }
-    }
-    
-    
     func getPlaceList (location: CLLocationCoordinate2D, range:Int, mapView:GMSMapView) {
         let centerPoint = "\(location.latitude),\(location.longitude)"
-        let range = 1000 * range
+        let range:Double = Double(1000 * range)
         let type = "cafe"
         let key = "AIzaSyB2VMdWQslynrxRgCA3vpWQAjqxvkKmCWk"
-
         let url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+        
         Alamofire.request(url,parameters:["key":key, "location":centerPoint, "radius":range,"type":type]).responseJSON { response in
             let json = JSON(response.result.value!)
             json["results"].forEach{(_,place) in
@@ -56,8 +45,29 @@ class SelectMichikusaViewController: UIViewController {
                 let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: placeLat, longitude: placeLng))
                 marker.map = mapView
             }
+            let camera = GMSCameraPosition.camera(withTarget: location, zoom: Float(self.getAppreciateZoomSize(distance: range)))
+            self.mapView.camera = camera
         }
     }
+    
+    func getAppreciateZoomSize(distance: Double) -> Int {
+        print(distance)
+        switch distance {
+        case 1..<1000:
+            return 16
+        case 1000..<5000:
+            return 14
+        case 5000..<10000:
+            return 13
+        case 10000..<20000:
+            return 12
+        case 20000..<50000:
+            return 7
+        case 50000..<1000000000000:
+            return 7
+        default:
+            return 10
+        }
+
+    }
 }
-
-
